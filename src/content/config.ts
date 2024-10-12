248dfed4fa8c1c1accba573dca79d58eb78f9fc3
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2024-10-12 12:05:14 -0400

    Updated to use Astro 5.0 beta loader

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,9 @@
-// Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
-// Define a `type` and `schema` for each collection
+import { glob } from 'astro/loaders';
+
+// Define a `loader` and `schema` for each collection
 const blogCollection = defineCollection({
-    type: 'content',
+    loader: glob({ pattern: '**\/[^_]*.md', base: "./src/content/blog" }),
     schema: z.object({
       title: z.string(),
       subtitle: z.string(),
