2e22b6185abd486effc401dc43e21ab94eed8d9a
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2024-10-12 11:51:36 -0400

    Upgraded to Astro 5 beta and Removed MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
     site: 'https://baisley.dev',
     integrations: [
         sitemap(),
-        mdx()
     ],
 });
