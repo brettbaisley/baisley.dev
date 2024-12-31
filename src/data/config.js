109525b0b6ce78bc0691a4c796931d94bf1c73eb
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2024-12-31 11:49:57 -0500

    Moved Menu to data file

---
+++ src/data/config.js
@@ -0,0 +1,4 @@
+// Global variables
+export const siteLang = "en-US";
+export const siteDirection = "ltr";
+export const siteDomain = "baisley.dev";
---
+++ src/data/menus.js
@@ -0,0 +1,22 @@
+export const mainMenu = [
+    { 
+        name: "Home",
+        url: "/"
+    },
+    { 
+        name: "About", 
+        url: "/about", 
+    },
+    { 
+        name: "Days", 
+        url: "/days", 
+    },
+    { 
+        name: "Blog",
+        url: "/blog"
+    },
+    { 
+        name: "Top Ten", 
+        url: "/top-ten", 
+    },
+];
